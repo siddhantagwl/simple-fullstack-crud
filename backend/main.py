3a85a543b987b6e90096269cf8239564b3d648a7
Siddhant Agarwal <siddhantagwl29@gmail.com>
2024-06-09 12:38:11 +0200

    added create contact route

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,29 @@ def home():
 # create end point: localhost:8080/create_contact
 # we need firstname, lastname and email and submit data to server,
 # so a post request
+@app.route("/create_contact", methods=["POST"])
+def create_contact():
+    # get the contact first that was submitted
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
+    email = request.json.get("email")
+
+    # validate if they exist or not
+    if not first_name or not last_name or not email:
+        return (jsonify({"message": "First Name, Last Name and email are mandatory"}), 400)
+    
+    # now we have valid fields and it can be passed to the DB class as constructor
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
+
+    # add to DB
+    try:
+        db.session.add(new_contact) # staging area, not really fully committed to DB yet
+        db.session.commit() # actually written to DB
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+
+    # 201 is response code for creating a new entry
+    return jsonify({"message": f"User {first_name} created"}), 201
 
 
 # read
@@ -19,7 +42,8 @@ def get_contacts():
     contacts = Contact.query.all() # returns python obj
     # convert to json now
     json_contacts = list(map(lambda x: x.to_json(), contacts))
-    return jsonify({"contacts": json_contacts})
+    # note: status code 200 is by default included
+    return jsonify({"contacts": json_contacts}, 200)
 
 # update
 
